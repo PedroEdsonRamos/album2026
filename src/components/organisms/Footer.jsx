f5d84726895d74501f17cc7a604e2dafa0445a3e
PedroEdsonRamos <pedroedsonengmec@gmail.com>
2026-05-27 19:02:45 -0300

    fix: adjust PTEC logo vertical alignment in footer

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -45,7 +45,7 @@ export function Footer() {
         </div>
       </div>
       <div
-        style={{ position: "absolute", right: 14, top: "50%", transform: "translateY(-59%)" }}
+        style={{ position: "absolute", right: 14, top: "50%", transform: "translateY(-58%)" }}
       >
         <PTECLogo height={58} />
       </div>
